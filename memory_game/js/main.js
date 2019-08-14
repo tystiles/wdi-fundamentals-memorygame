d670b82ff20716d665d948c1aa7feaf0e633b16b
tyrone <tyrone.stiles@yahoo.com>
2019-08-13 21:44:18 -0400

    created game logic - Objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,31 @@
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png" 
+	},
+
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png" 
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png" 
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png" 
+	}
+
+];
+
 
 var cardsInPlay = [];
 
@@ -9,11 +34,11 @@ function checkForMatch(){
 
 	if (cardsInPlay[0] === cardsInPlay[1])
 
-		console.log("You found a match!");
+		alert("You found a match!");
 
 	else
 
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 
 }
 
@@ -21,10 +46,12 @@ function checkForMatch(){
 function flipCard(cardId){
 
 
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	
 
-
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
 
 	if (cardsInPlay.length === 2)
